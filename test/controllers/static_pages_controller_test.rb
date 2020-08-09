01f108cbf696674ca66706bfaf1e023ea0220c37
julianfssen <julianfssen@gmail.com>
2020-08-09 19:23:41 +0800

    Added basic user model and authentication

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,7 +6,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get home' do
-    get static_pages_home_url
+    get root_url
     assert_response :success
     assert_select 'title', "Home | #{@base_title}"
   end
@@ -22,4 +22,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select 'title', "About | #{@base_title}"
   end
+
+  test 'should get contact' do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select 'title', "Contact | #{@base_title}"
+  end
+
 end
