71210a4e1eeb20e32ae678c4d35c8cf1be0463ae
Julian Foo <julianfssen@gmail.com>
2020-08-12 13:24:35 +0800

    Fixed migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_063602) do
+ActiveRecord::Schema.define(version: 2020_08_12_052345) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_digest"
   end
