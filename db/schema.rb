dd2e61fcc8903a4c26729542762dd5f4db241962
Julian Foo <julianfssen@gmail.com>
2020-08-13 13:14:54 +0800

    Added account activation feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_12_060303) do
+ActiveRecord::Schema.define(version: 2020_08_12_070410) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,9 @@ ActiveRecord::Schema.define(version: 2020_08_12_060303) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: true
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
   end
 
 end
